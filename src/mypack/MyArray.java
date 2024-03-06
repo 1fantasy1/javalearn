650d9b72ad83f5f1f5969961f9acc5deb8434c1d
1fantasy1 <127208503+1fantasy1@users.noreply.github.com>
2024-03-06 19:05:16 +0800

    empty

---
+++ src/mypack/MyArray.java
@@ -0,0 +1,21 @@
+package mypack;
+
+public class MyArray {
+    public static void main(String[] args) {
+        int[] myArray=new int[10];
+        myArray[0]=0;
+        myArray[1]=1;
+        myArray[2]=2;
+        myArray[9]=9;
+        System.out.println("Index\t\tValue");
+        for (int i=0;i<myArray.length; i++) {
+            System.out.println(i + "\t\t" +myArray[i]);
+        }
+        System.out.print("Values:");
+        for (int i :myArray) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+//        myArray[10]=100;
+    }
+}
---
+++ src/mypack/MyFirstProgram.java
@@ -0,0 +1,9 @@
+package mypack;
+
+public class MyFirstProgram {
+    private String myName = "小明";
+    public  void  display()
+    {
+        System.out.println();
+    }
+}
